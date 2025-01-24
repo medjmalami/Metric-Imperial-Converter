ecc9f3adb59d9412d7a09d767612c45a702c0f9e
amine <jmalmohamedamine1@gmail.com>
2025-01-24 16:32:24 +0100

    first commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,39 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+
+    if (!initNum && !initUnit) {
+      return res.send("invalid number and unit");
+    } else if (!initNum) {
+      return res.send("invalid number");
+    } else if (!initUnit) {
+      return res.send("invalid unit");
+    }
+
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string: toString,
+    });
+  });
 };
